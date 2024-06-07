6bf75ca61d85e8b43bd0966a57b13e4f64ad6a34
naixx <r.chekan@gmail.com>
2024-06-07 12:48:00 +0300

    Fix highlights issue when ramping went up instead of down Add additional logging Infrastructure for tests

--- test.js
+++ test.js
@@ -1,16 +1,20 @@
 //var intervalometer = require('./intervalometer/intervalometer');
 var exp = require('./intervalometer/exposure');
 var tv = require('./intervalometer/time-value');
-var datajs = require('./logs/data')
+var datajs = require('./work/tl-217/data')
+// var datajs = require('./logs/data')
 
 
 console.log(exp);
 
-exp.init(0, 0, -1.5, 1, true, "./logs/data2.js");
+exp.init(-7, 19, -1, 0, true, "./logs/data2.js");
+
+exp.status.rampEv = 3.3333333333333335
 
 datajs.data.forEach(value => {
 
-    exp.calculate("lrt", "auto", value.currentEv, value.lastPhotoLum, value.lastPhotoHistogram, 0, 0);
+    var rate = exp.calculate("lrt", "auto", value.currentEv, value.lastPhotoLum, value.lastPhotoHistogram, -7, 19);
+    console.log("rate = ", rate);
 
 })
 
